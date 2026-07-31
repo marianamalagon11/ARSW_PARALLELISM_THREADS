8069c0a1de58d04de0ba7b32af1a7312a52174f3
unknown <paula.lozano-c@labinfo.is.escuelaing.edu.co>
2026-07-31 12:03:14 -0500

    New feature partOne

--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -9,16 +9,25 @@ package edu.eci.arsw.threads;
 
 public class CountThreadsMain {
     
-    public static void main(String args[]){
-        Thread thread1 = new Thread(new CountThread(new int[]{0,99}));
-        Thread thread2 = new Thread(new CountThread(new int[]{99,199}));
-        Thread thread3 = new Thread(new CountThread(new int[]{200,299}));
+    public static void main(String args[]) {
+        int start = 0;
+        int end = 300;
+
+        private divide(start, end) {
+            double interval = (end - start) mod 3;
+            double t1_end = start + interval;
+            double t2_start = t1_end + 1;
+            double t2_end = t1_end + interval;
+            double t3_start = t2_end + 1; 
+        }
+
+        Thread thread1 = new Thread(new CountThread(new int[]{start, t1_end}));
+        Thread thread2 = new Thread(new CountThread(new int[]{t2_start, t2_end}));
+        Thread thread3 = new Thread(new CountThread(new int[]{t3_start,end}));
 
         thread1.run();
         thread2.run();
         thread3.run();
-
-
     }
 
 }
