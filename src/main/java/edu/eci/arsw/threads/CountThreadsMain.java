ea7388c8a508c2fb261f6ad091313f782ba1a6f0
Valentina-33 <lozano.paula1021@gmail.com>
2026-07-31 19:40:44 -0500

    Tweaking

--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -23,10 +23,18 @@ public class CountThreadsMain {
         int currentStart = start;
         
         for (int i = 0; i < 3; i++) {
-            int currentEnd = currentStart + intervalSize - 1;
 
-            Thread thread = new Thread(new CountThread(currentStart, currentEnd), "Hilo " + (i+1));
+            int currentEnd;
+            if (i == 2) {
+                currentEnd = end;
+            } else {
+                currentEnd = currentStart + intervalSize - 1;
+            }
 
+            Thread thread = new Thread(
+                new CountThread(currentStart, currentEnd), // Task
+                "Hilo " + (i+1) // Thread name
+            );
             thread.start();
 
             currentStart = currentEnd + 1;
